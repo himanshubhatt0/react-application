179ec434958ba247d8621f620de56fd422c9ba00
himanshu <himanshubhatt2015@gmail.com>
2020-09-19 16:11:42 +0530

    second assignment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Card,CardBody,CardImg,CardText,CardTitle} from "reactstrap";
-
+import {Card,CardBody,CardImg,CardText,CardTitle,BreadcrumbItem,Breadcrumb} from "reactstrap";
+import {Link} from 'react-router-dom';
     function RenderDish({dish}){
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -15,39 +15,57 @@ import {Card,CardBody,CardImg,CardText,CardTitle} from "reactstrap";
                   
             );
     }
-    function RenderComments({comments}){
-        if(comments!=null){
-            const commentlistitem=comments.map((comment) => {
-                return(
-                    <li key={comment.id}>
+    function RenderComments({comment}){
+        if(comment != null)
+        {
+            const comms =comment.map((comment) => {
+                return (
+                <ul  key={comment.id} className="list-unstyled">
+                    <li>
                         <p>{comment.comment}</p>
                         <p>--{comment.author},{new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                     </li>
+                    </ul>
+                    
                 );
             });
             return(
                 <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        {commentlistitem}
-                    </ul>
+                <h4>Comments</h4>
+                    {comms}
                 </div>
-            );
+
+            );        
         }
         else{
             return(
-            <div></div>
+                <div></div>
             );
         }
     }
+   
 
     const Dishdetail=(props) =>{
         if(props.dish != null){
         return(
             <div className="container">
+                 <div className="row">
+                    <Breadcrumb>
+
+                    <BreadcrumbItem>
+                    <Link to='/menu'>Menu</Link>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>
+                        {props.dish.name}
+                    </BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3> {props.dish.name}</h3><hr/>
+                    </div>
+                </div>
             <div className="row">
                <RenderDish dish={props.dish}/>
-               <RenderComments comments={props.dish.comments}/>
+               <RenderComments comment={props.comments}/>
             </div>
             </div>
         );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Menu from "./MenuComponent";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import {Switch,Route,Redirect} from 'react-router-dom';
 
 
@@ -36,6 +37,12 @@ class Main extends Component{
         />
       );
     }
+    const DishWithId=({match})=>{
+      return(
+          <Dishdetail dish={this.state.dishes.filter((dish)=> dish.id === parseInt(match.params.dishId,10))[0]} comments={this.state.comments.filter((comment)=> comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+
+    }
     
     
     return(
@@ -44,7 +51,9 @@ class Main extends Component{
        <Switch>
        <Route path="/home" component={HomePage}/>
        <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
+       <Route path="/menu/:dishId" component={DishWithId}/>
        <Route exact path="/contactus" component={Contact}/>
+       <Route exect path="/aboutus" component={()=><About leaders={this.state.leaders}/> }/>
        <Redirect to="/home"/>
        </Switch>
         <Footer/>
